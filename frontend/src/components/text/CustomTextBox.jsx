7db448040d2d2878750dee6462b4fe5081e727f4
tylerflowers <rsdcmain@gmail.com>
2025-04-15 18:54:58 -0400

    adding customTextBox

---
+++ frontend/src/components/text/CustomTextBox.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { TextField, Typography, Box } from '@mui/material';
+
+const CustomTextBox = ({
+  fontSize = '1rem',
+  font = 'Arial',
+  bold = false,
+  height = '40px',
+  width = '300px',
+  color = 'black', // optional
+  error = false,
+  errorMessage = '',
+  value,
+  onChange,
+  onBlur,
+}) => {
+  return (
+    <Box sx={{ width, marginBottom: '1rem' }}>
+      {error && errorMessage && (
+        <Typography
+          sx={{
+            color: 'red',
+            fontSize: '0.875rem',
+            marginBottom: '4px',
+            fontFamily: font,
+          }}
+        >
+          {errorMessage}
+        </Typography>
+      )}
+      <TextField
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        fullWidth
+        error={error}
+        variant="outlined"
+        InputProps={{
+          sx: {
+            fontSize,
+            fontFamily: font,
+            fontWeight: bold ? 'bold' : 'normal',
+            color,
+            height,
+          },
+        }}
+      />
+    </Box>
+  );
+};
+
+export default CustomTextBox;
