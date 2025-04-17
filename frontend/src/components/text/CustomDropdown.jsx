8b83b0d734f131fcaab29815c777b42e0ccbf049
wbermel3rd <willathrill@gmail.com>
2025-04-16 23:41:28 -0400

    Create CustomDropdown
    Line 32 - If backend is connected, use Flask / Jinja here.

---
+++ frontend/src/components/text/CustomDropdown.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Box, FormControl, InputLabel, Select, MenuItem, FormHelperText } from '@mui/material';
+
+const CustomDropdown = ({
+  label = 'Select an option',
+  options = [], // array of { label: 'Option 1', value: 'option1' }
+  fontSize = '1rem',
+  font = 'Arial',
+  bold = false,
+  height = '40px',
+  width = '300px',
+  color = 'black',
+  error = false,
+  errorMessage = '',
+  value,
+  onChange,
+  onBlur,
+}) => {
+  return (
+    <Box>
+      <FormControl
+        fullWidth error={error} sx={{width}}
+      >
+        <InputLabel
+          sx={{fontFamily: font, fontWeight: bold ? 'bold' : 'normal', fontSize, color}}
+        >
+          {label}
+        </InputLabel>
+        <Select value={value} onChange={onChange} onBlur={onBlur} label={label} 
+                sx={{fontSize,fontFamily: font, fontWeight: bold ? 'bold' : 'normal', color, height}}
+        >
+          {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+        {error && errorMessage && (
+          <FormHelperText>{errorMessage}</FormHelperText>
+        )}
+      </FormControl>
+    </Box>
+  );
+};
+
+export default CustomDropdown;
