92ae6a7e14ac2f3589e9a85cea8603400fee3ab9
tylerflowers <rsdcmain@gmail.com>
2025-04-21 18:29:09 -0400

    Creating SocialIconLink & Link Footer
    added materialUI library, created socialIconLink and created the footer component which uses them.

---
+++ frontend/src/components/pageComponents/LinkFooter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import SocialIconLink from '../buttons/SocialIconLink';
+import { iconMap } from '../../utils/iconRegistry';
+
+const LinkFooter = ({ backgroundColor = '#000', iconColor = '#aaa', hoverColor = '#fff', icons = [] }) => {
+  return (
+    <Box component="footer" bgcolor={backgroundColor} py={2} display="flex" justifyContent="center" gap={1}>
+      {icons.map(({ name, href }) => {
+        const IconComponent = iconMap[name];
+        if (!IconComponent) return null;
+        return (
+          <SocialIconLink key={name} IconComponent={IconComponent} href={href} label={name} color={iconColor} hoverColor={hoverColor} />
+        );
+      })}
+    </Box>
+  );
+};
+
+export default LinkFooter;
