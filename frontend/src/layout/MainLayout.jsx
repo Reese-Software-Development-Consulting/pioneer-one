e01fe5b783f32d44942852502c7900243ac99530
tylerflowers <rsdcmain@gmail.com>
2025-05-07 17:59:17 -0400

    Created New Custom Footer & Fixed Horizontally scrollable issue
     - Added New Footer Element
     - Added Fonts
     - Fixed Horizontally Scrollable issue

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CustomHeader from "../components/pageComponents/CustomHeader";
-import LinkFooter from "../components/pageComponents/LinkFooter";
+import CustomFooter from "../components/pageComponents/CustomFooter";
 import NavLinkPositionEnum from "../enums/NavLinkPositionEnum";
 
 import ProfileImage from "../assets/test_images/profile-image.jpg";
@@ -39,26 +39,9 @@ const MainLayout = ({ children }) => {
   return (
     <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <CustomHeader
-        title="Carl's Construction"
-        titleFont="'Poppins', sans-serif"
-        titleColor="#000"
-        titleTextSize="25px"
-        profileImage={ProfileImage}
-        locations={locations}
-        phoneNumbers={phoneNumbers}
-        socialLinks={socialLinks}
-        isLinkBarVisible={true}
-        topLinkBackgroundColor="#000"
-        topLinkTextColor="#fff"
-        navItems={navItems}
-        navBarBackgroundColor="white"
-        navBarTextColor="black"
-        navBarItemFont="'Poppins', sans-serif"
-        navBarItemSpacing="6rem"
-        navBarItemHoverColor="#FFB400"
-        navBarTextSize="20px"
-        isSticky={true}
-        navLinkPosition={NavLinkPositionEnum.CENTER}
+        title="Carl's Construction" titleFont="'Poppins', sans-serif" titleColor="#000" titleTextSize="25px" profileImage={ProfileImage} locations={locations} phoneNumbers={phoneNumbers}
+        socialLinks={socialLinks} isLinkBarVisible={true} topLinkBackgroundColor="#000" topLinkTextColor="#fff" navItems={navItems} navBarBackgroundColor="white" navBarTextColor="black"
+        navBarItemFont="'Poppins', sans-serif" navBarItemSpacing="6rem" navBarItemHoverColor="#FFB400" navBarTextSize="20px" isSticky={true} navLinkPosition={NavLinkPositionEnum.CENTER}
         headerHeight={`${headerHeight}px`}
       />
 
@@ -66,12 +49,16 @@ const MainLayout = ({ children }) => {
         {children}
       </main>
 
-      <LinkFooter
-        backgroundColor="black"
-        iconColor="black"
-        hoverColor="#FFB400"
-        icons={socialLinks}
-      />
+      <CustomFooter
+        height="320px" backgroundColor="#333" sticky={false} companyLogo={ProfileImage} showScrollToTopButton={true} scrollToTopButtonColor="#111" scrollToTopArrowColor="#fff"
+        textFont="var(--font-poppins)" companyName="Carl's Construction" isCompanyNameBold={true} email="info@carl.com" phone="(704) 532-3836" addressLine1="P.O. Box 481179"
+        addressLine2="Charlotte, NC 28269" showBottomTabBar={true} bottomTabBarBackgroundColor="#000"
+        socialLinks={[
+            { platform: 'Instagram', href: 'https://instagram.com' },
+            { platform: 'LinkedIn', href: 'https://linkedin.com' },
+            { platform: 'Twitter', href: 'https://twitter.com' },
+        ]}
+    />
     </div>
   );
 };
