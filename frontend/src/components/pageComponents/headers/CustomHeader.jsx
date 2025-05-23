f551c658430e2f10d72936a2035be8a708e5f16e
tylerflowers <rsdcmain@gmail.com>
2025-05-23 01:24:50 -0400

    updated header to be responsive

--- frontend/src/components/pageComponents/headers/CustomHeader.jsx
+++ frontend/src/components/pageComponents/headers/CustomHeader.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Box, Avatar, Button, Menu, MenuItem } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Avatar,
+  Button,
+  Menu,
+  MenuItem,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  useMediaQuery,
+  useTheme
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import PhoneIcon from '@mui/icons-material/Phone';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { iconMap } from '../../../utils/iconRegistry';
 import SocialIconLink from '../../buttons/SocialIconLink';
@@ -35,6 +53,10 @@ const CustomHeader = ({
 }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [dropdownItems, setDropdownItems] = useState([]);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
   const handleOpenDropdown = (event, items) => {
     setAnchorEl(event.currentTarget);
@@ -46,16 +68,22 @@ const CustomHeader = ({
     setDropdownItems([]);
   };
 
+  const toggleDrawer = (open) => () => {
+    setDrawerOpen(open);
+  };
+
   const justifyContentMap = {
     [NavLinkPositionEnum.LEFT]: 'flex-start',
     [NavLinkPositionEnum.CENTER]: 'center',
     [NavLinkPositionEnum.RIGHT]: 'flex-end',
   };
 
+  const finalHeaderHeight = isMobile ? '60px' : headerHeight;
+
   return (
     <Box sx={{ width: '100%' }}>
       {/* Top Link Bar */}
-      {isLinkBarVisible && (
+      {isLinkBarVisible && !isMobile && (
         <Box
           sx={{
             width: '100%',
@@ -111,8 +139,8 @@ const CustomHeader = ({
           backgroundColor: navBarBackgroundColor,
           color: navBarTextColor,
           boxShadow: 'none',
-          height: headerHeight,
-          top: isSticky && isLinkBarVisible ? '40px' : '0px',
+          height: finalHeaderHeight,
+          top: isSticky && !isMobile && isLinkBarVisible ? '40px' : '0px',
           zIndex: 1100,
           justifyContent: 'center',
         }}
@@ -135,74 +163,96 @@ const CustomHeader = ({
             </Typography>
           </Box>
 
-          {/* Center/Right (Nav Links) */}
-          <Box
-            sx={{
-              flex: 1,
-              display: 'flex',
-              justifyContent: justifyContentMap[navLinkPosition],
-              alignItems: 'center',
-              gap: navBarItemSpacing,
-            }}
-          >
-            {navItems.map((item, index) => (
-              item.dropdownItems && item.dropdownItems.length > 0 ? (
-                <Button
-                  key={index}
-                  onClick={(e) => handleOpenDropdown(e, item.dropdownItems)}
-                  endIcon={<ArrowDropDownIcon />}
-                  sx={{
-                    color: navBarTextColor,
-                    fontWeight: 'bold',
-                    fontFamily: navBarItemFont,
-                    fontSize: navBarTextSize,
-                    '&:hover': {
-                      color: navBarItemHoverColor,
-                    }
-                  }}
-                >
-                  {item.title}
-                </Button>
-              ) : (
-                <Button
-                  key={index}
-                  component={Link}
-                  to={item.link}
-                  sx={{
-                    color: navBarTextColor,
-                    fontWeight: 'bold',
-                    fontFamily: navBarItemFont,
-                    fontSize: navBarTextSize,
-                    '&:hover': {
-                      color: navBarItemHoverColor,
-                    }
-                  }}
-                >
-                  {item.title}
-                </Button>
-              )
-            ))}
-          </Box>
+          {/* Right (Nav Links or Menu Icon) */}
+          {isMobile ? (
+            <IconButton edge="end" color="inherit" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <Box
+              sx={{
+                flex: 1,
+                display: 'flex',
+                justifyContent: justifyContentMap[navLinkPosition],
+                alignItems: 'center',
+                gap: navBarItemSpacing,
+              }}
+            >
+              {navItems.map((item, index) =>
+                item.dropdownItems && item.dropdownItems.length > 0 ? (
+                  <Button
+                    key={index}
+                    onClick={(e) => handleOpenDropdown(e, item.dropdownItems)}
+                    endIcon={<ArrowDropDownIcon />}
+                    sx={{
+                      color: navBarTextColor,
+                      fontWeight: 'bold',
+                      fontFamily: navBarItemFont,
+                      fontSize: navBarTextSize,
+                      '&:hover': {
+                        color: navBarItemHoverColor,
+                      }
+                    }}
+                  >
+                    {item.title}
+                  </Button>
+                ) : (
+                  <Button
+                    key={index}
+                    component={Link}
+                    to={item.link}
+                    sx={{
+                      color: navBarTextColor,
+                      fontWeight: 'bold',
+                      fontFamily: navBarItemFont,
+                      fontSize: navBarTextSize,
+                      '&:hover': {
+                        color: navBarItemHoverColor,
+                      }
+                    }}
+                  >
+                    {item.title}
+                  </Button>
+                )
+              )}
+            </Box>
+          )}
         </Toolbar>
       </AppBar>
 
-      {/* Dropdown Menu */}
-      <Menu
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleCloseDropdown}
-      >
+      {/* Dropdown Menu (desktop) */}
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleCloseDropdown}>
         {dropdownItems.map((dropItem, idx) => (
-          <MenuItem
-            key={idx}
-            component={Link}
-            to={dropItem.link}
-            onClick={handleCloseDropdown}
-          >
+          <MenuItem key={idx} component={Link} to={dropItem.link} onClick={handleCloseDropdown}>
             {dropItem.title}
           </MenuItem>
         ))}
       </Menu>
+
+      {/* Drawer Menu (mobile) */}
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <Box sx={{ width: 250, p: 2 }} role="presentation" onClick={toggleDrawer(false)}>
+          <List>
+            {navItems.map((item, index) =>
+              item.dropdownItems && item.dropdownItems.length > 0 ? (
+                item.dropdownItems.map((dropItem, subIdx) => (
+                  <ListItem key={`${index}-${subIdx}`} disablePadding>
+                    <ListItemButton component={Link} to={dropItem.link}>
+                      <ListItemText primary={dropItem.title} />
+                    </ListItemButton>
+                  </ListItem>
+                ))
+              ) : (
+                <ListItem key={index} disablePadding>
+                  <ListItemButton component={Link} to={item.link}>
+                    <ListItemText primary={item.title} />
+                  </ListItemButton>
+                </ListItem>
+              )
+            )}
+          </List>
+        </Box>
+      </Drawer>
     </Box>
   );
 };
