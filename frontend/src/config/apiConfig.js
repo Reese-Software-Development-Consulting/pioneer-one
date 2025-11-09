9d04c4f74ee127cfa2015c13d0627762ec2c61b2
tylerflowers <rsdcmain@gmail.com>
2025-11-08 21:09:16 -0500

    updates to fix routing

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,7 +1,7 @@
 //FOR DEV
-const API_BASE_URL = 'http://127.0.0.1:5000/'; // Change this for prod
-const CONTACT_ENDPOINT = `${API_BASE_URL}/api/contact`;
+// const API_BASE_URL = 'http://127.0.0.1:5000/'; // Change this for prod
+// const CONTACT_ENDPOINT = `${API_BASE_URL}/api/contact`;
 
-// const CONTACT_ENDPOINT = `/api/contact`;
+const CONTACT_ENDPOINT = `/api/contact`;
 
 export { CONTACT_ENDPOINT };
